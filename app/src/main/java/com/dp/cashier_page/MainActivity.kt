09ec89eedb64a88b6037bb82efe215ca8508e96a
johnbrix <pomoyjohnbrix@gmail.com>
2022-02-05 03:51:26 +0800

    fixed dsiable autofocus when not needed

--- app/src/main/java/com/dp/cashier_page/MainActivity.kt
+++ app/src/main/java/com/dp/cashier_page/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dp.cashier_page
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.item_product_list)
         supportActionBar?.title = "Products"
 
+        var cash = findViewById(R.id.cash) as TextInputEditText
+        var cashLayout = findViewById(R.id.cashLayout) as TextInputLayout
+        var screen = findViewById(R.id.screen) as LinearLayout
 
+
+        screen.setOnClickListener{
+            cash.clearFocus();
+        }
     }
 
 
